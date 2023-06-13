c25e9a4d2d26aaf56ef8d541d1713accc859b652
Kevin Kipkemboi <theptjhub@gmail.com>
2023-06-13 11:57:30 -0700

    added public, routes and views

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const PORT = process.env.PORT || 3500
 
+app.use('/', express.static(path.join(__dirname, '/public')))
+// telling express where to find the file. 
+
+app.use('/', require('./routes/root'))
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
